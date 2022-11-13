49b621b0e2313a9ff95991ccefda01d6bd88a4ff
James Randall <james@accidentalfish.com>
2022-11-13 20:53:22 +0000

    Some bricks

--- Farkanoid/Program.fs
+++ Farkanoid/Program.fs
@@ -5,11 +5,21 @@ open GameEngine.Model
 let sceneDefinition:SceneDefinition =
   { SpriteSets =[
       SpriteSet.FromImage "Images/redBrick.png"
+      SpriteSet.FromImage "Images/yellowBrick.png"
       SpriteSet.FromImage "Images/greenBrick.png"
     ]
     GameObjects = [
-      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 100 ; Y = 100 } }
+      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 2 } }
+      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 2 } }
+      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 2 } }
+      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 48 } }
+      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 48 } }
+      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 48 } }
+      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 94 } }
+      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 94 } }
+      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 94 } }
+      
     ]
   }
 
-printfn "Hello from F#"
+GameEngine.Startup.create "Farkanoid" 800 600 sceneDefinition
--- GameEngine/GameEngine.fs
+++ GameEngine/GameEngine.fs
@@ -11,30 +11,32 @@ type RenderData =
     SpriteRenderer: Vector2 -> Vector2 -> Texture.T -> unit
     Scene: Scene
   }
-let mutable renderDataOption = None
 
-let load (window:IWindow) (sceneDefinition:SceneDefinition) _ =
-  let gl = Silk.NET.OpenGL.GL.GetApi(window)
-  
-  let scene = SceneBuilder.createScene gl sceneDefinition
-  renderDataOption <- Some
-    { Gl = gl
-      SpriteRenderer = SpriteRenderer.create gl (float32 window.Size.X) (float32 window.Size.Y)
-      Scene = scene
-    }
-  ()
-  
-let updateAndRender (frameTime:double) =
-  match renderDataOption with
-  | Some rd ->
-    rd.Scene.GameObjects
-    |> List.iter(fun gameObject ->
-      rd.SpriteRenderer gameObject.Position.AsVector2 gameObject.Size.AsVector2 gameObject.Sprite.CurrentTexture
-    )        
-  | None ->
-    () // still loading
 
 let create windowTitle windowWidth windowHeight sceneDefinition =
+  let mutable renderDataOption = None
+
+  let load (window:IWindow) (sceneDefinition:SceneDefinition) _ =
+    let gl = Silk.NET.OpenGL.GL.GetApi(window)
+    
+    let scene = SceneBuilder.createScene gl sceneDefinition
+    renderDataOption <- Some
+      { Gl = gl
+        SpriteRenderer = SpriteRenderer.create gl (float32 window.Size.X) (float32 window.Size.Y)
+        Scene = scene
+      }
+    ()
+    
+  let updateAndRender (frameTime:double) =
+    match renderDataOption with
+    | Some rd ->
+      rd.Scene.GameObjects
+      |> List.iter(fun gameObject ->
+        rd.SpriteRenderer gameObject.Position.AsVector2 gameObject.Size.AsVector2 gameObject.Sprite.CurrentTexture
+      )        
+    | None ->
+      () // still loading
+  
   let mutable options = WindowOptions.Default
   options.Size <- Vector2D(windowWidth,windowHeight)
   options.Title <- windowTitle
