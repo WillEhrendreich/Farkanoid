10889ed01f2bcfb837ee8ed67b9fcfacb5bde44c
James Randall <james@accidentalfish.com>
2022-11-13 21:01:12 +0000

    Some more bricks

--- Farkanoid/Program.fs
+++ Farkanoid/Program.fs
@@ -2,24 +2,30 @@
 
 open GameEngine.Model
 
-let sceneDefinition:SceneDefinition =
+let sceneDefinition =
+  let brickWidth = 92
+  let brickHeight = 44
+  
+  let brickRow spriteSetName y =
+   {0..7}
+    |> Seq.map(fun i ->
+      { Id = "" ; SpriteSetName = spriteSetName ; SpriteAnimationSequence = [0] ; Position = { X = 2 + i * (brickWidth + 2); Y = y } }
+    )
+    |> Seq.toList
+    
+  let rows = [ "redBrick" ; "yellowBrick" ; "greenBrick" ]
+  
   { SpriteSets =[
       SpriteSet.FromImage "Images/redBrick.png"
       SpriteSet.FromImage "Images/yellowBrick.png"
       SpriteSet.FromImage "Images/greenBrick.png"
     ]
-    GameObjects = [
-      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 2 } }
-      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 2 } }
-      { Id = "" ; SpriteSetName = "redBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 2 } }
-      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 48 } }
-      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 48 } }
-      { Id = "" ; SpriteSetName = "yellowBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 48 } }
-      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 2 ; Y = 94 } }
-      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 96 ; Y = 94 } }
-      { Id = "" ; SpriteSetName = "greenBrick" ; SpriteAnimationSequence = [0] ; Position = { X = 190 ; Y = 94 } }
-      
-    ]
+    GameObjects =
+      rows
+      |> List.mapi(fun row color ->
+        brickRow color (2 + (brickHeight + 2) * row)
+      )
+      |> List.concat
   }
 
 GameEngine.Startup.create "Farkanoid" 800 600 sceneDefinition
